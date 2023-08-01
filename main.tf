a4db0212eb24d4e8a4a0cb4c9153ee04f1e4bca6
Simon Buckle <simon.buckle@gmail.com>
2023-08-01 18:37:23 +0100

    Added a dependency on the public access block

--- main.tf
+++ main.tf
@@ -18,6 +18,10 @@ resource "aws_s3_bucket_public_access_block" "hls" {
 resource "aws_s3_bucket_policy" "allow_public_access" {
   bucket = aws_s3_bucket.hls.id
   policy = data.aws_iam_policy_document.allow_public_access.json
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.hls
+  ]
 }
 
 data "aws_iam_policy_document" "allow_public_access" {
